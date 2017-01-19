65700a2ceec0f23d94ebe94d0cad6c1f66200ef6
Philip Ochu <pochu@eminenttechnology.com>
2017-01-18 19:30:28 -0600

    updates

---
+++ src/core/CodeTorch.Backend/PermissionManager.cs
@@ -0,0 +1,137 @@
+﻿using CodeTorch.Abstractions;
+using CodeTorch.Core;
+using CodeTorch.Core.Interfaces;
+using CodeTorch.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTorch.Backend
+{
+   
+    public class PermissionManager
+    {
+        readonly IConfigurationStore Store;
+        readonly ILog Log;
+
+        public PermissionManager(IConfigurationStore store, ILogManager logger)
+        {
+            Store = store;
+            Log = logger.GetLogger(typeof(DataCommandManager));
+        }
+
+        public async Task<List<Permission>> Search(string Category, string Name)
+        {
+            List<Permission> retVal = null;
+
+            retVal = await Store.GetItems<Permission>();
+
+            //filter by name
+            if (!String.IsNullOrEmpty(Name))
+            {
+                retVal = retVal
+                    .Where(l => l.Name.ToLower().Contains(Name.ToLower()))
+                    .ToList<Permission>();
+            }
+
+            //filter by category
+            if (!String.IsNullOrEmpty(Name))
+            {
+                retVal = retVal
+                    .Where(l => l.Category.ToLower().Contains(Category.ToLower()))
+                    .ToList<Permission>();
+            }
+
+            //order items by name
+            retVal = retVal
+                .OrderBy(l => l.Category)
+                .ThenBy(l=>l.Name)
+                .ToList<Permission>();
+
+            return retVal;
+        }
+
+       
+
+        public async Task Delete(string Name)
+        {
+            var item = await Store.GetItem<Permission>(Name);
+
+            if (item != null)
+            {
+                await Store.Delete<Permission>(Name);
+            }
+            else
+            {
+                throw new Exception(String.Format("Permission {0} does not exist in configuration", Name));
+            }
+
+
+        }
+
+        public async Task<string> Add
+        (
+            string Name, string Category, string Description
+
+        )
+        {
+            Permission retVal = null;
+           
+            //check for existence
+            if (await Store.Exists<Permission>(Name))
+            {
+                throw new Exception(String.Format("Permission {0} already exists", Name));
+            }
+
+            retVal = new Permission();
+
+            
+            retVal.Name = Name;
+            retVal.Category = Category;
+            retVal.Description = Description;
+          
+
+
+            retVal = await Store.Add(Name, retVal);
+
+            return retVal.Name;
+        }
+
+        public async Task<Permission> GetByName(string Name)
+        {
+            Permission retVal = null;
+
+            retVal = await Store.GetItem<Permission>(Name);
+
+            return retVal;
+        }
+
+       
+
+        public async Task Update(
+            string Name, string Category, string Description
+            )
+        {
+            var retVal = await Store.GetItem<Permission>(Name);
+
+            if (retVal != null)
+            {
+                retVal.Category = Category;
+                retVal.Description = Description;
+
+                await Store.Save(Name, retVal);
+            }
+            else
+            {
+                throw new Exception(String.Format("Permission {0} does not exist in configuration", Name));
+            }
+
+
+        }
+
+      
+
+    }
+}
---
+++ src/core/CodeTorch.Backend/ScreenManager.cs
@@ -0,0 +1,156 @@
+﻿using CodeTorch.Abstractions;
+using CodeTorch.Core;
+using CodeTorch.Core.Interfaces;
+using CodeTorch.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeTorch.Backend
+{
+   
+    public class ScreenManager
+    {
+        readonly IConfigurationStore Store;
+        readonly ILog Log;
+
+        public ScreenManager(IConfigurationStore store, ILogManager logger)
+        {
+            Store = store;
+            Log = logger.GetLogger(typeof(DataCommandManager));
+        }
+
+        public async Task<List<Screen>> Search(string Name)
+        {
+            List<Screen> retVal = null;
+
+            retVal = await Store.GetItems<Screen>();
+
+            //filter by name
+            if (!String.IsNullOrEmpty(Name))
+            {
+                retVal = retVal
+                    .Where(l => l.Name.ToLower().Contains(Name.ToLower()))
+                    .ToList<Screen>();
+            }
+
+            //order items by name
+            retVal = retVal
+                .OrderBy(l => l.Name)
+                .ToList<Screen>();
+
+            return retVal;
+        }
+
+        public async Task Delete(string Name)
+        {
+            var item = await Store.GetItem<Screen>(Name);
+
+            if (item != null)
+            {
+                await Store.Delete<Screen>(Name);
+            }
+            else
+            {
+                throw new Exception(String.Format("Screen {0} does not exist in configuration", Name));
+            }
+
+
+        }
+
+        public async Task<string> Add
+        (
+            string Name, string Url, string DisplayField, string ValueField,
+            string DataCommand, string DataCommandParameter,
+            int Height, int Width
+
+        )
+        {
+            Screen retVal = null;
+           
+            //check for existence
+            if (await Store.Exists<Screen>(Name))
+            {
+                throw new Exception(String.Format("Screen {0} already exists", Name));
+            }
+
+            retVal = new Screen();
+
+            if ((Height == 0) && (Width == 0))
+            {
+                //default 
+                Width = 512;
+                Height = 512;
+            }
+            
+            //retVal.Name = Name;
+            //retVal.Url = Url;
+            //retVal.DisplayField = DisplayField;
+            //retVal.ValueField = ValueField;
+            //retVal.DataCommand = DataCommand;
+            //retVal.DataCommandParameter = DataCommandParameter;
+            //retVal.Height = Height;
+            //retVal.Width = Width;
+
+
+            retVal = await Store.Add(Name, retVal);
+
+            return retVal.Name;
+        }
+
+        public async Task<Screen> GetByName(string Name)
+        {
+            Screen retVal = null;
+
+            retVal = await Store.GetItem<Screen>(Name);
+
+            return retVal;
+        }
+
+        public async Task<Screen> GetSections(string Name)
+        {
+            Screen retVal = null;
+
+            retVal = await Store.GetItem<Screen>(Name);
+
+            return retVal;
+        }
+
+
+
+        public async Task Update(
+            string Name, string Url, string DisplayField, string ValueField,
+            string DataCommand, string DataCommandParameter,
+            int Height, int Width
+            )
+        {
+            var retVal = await Store.GetItem<Screen>(Name);
+
+            if (retVal != null)
+            {
+               
+                //retVal.Name = Name;
+                //retVal.Url = Url;
+                //retVal.DisplayField = DisplayField;
+                //retVal.ValueField = ValueField;
+                //retVal.DataCommand = DataCommand;
+                //retVal.DataCommandParameter = DataCommandParameter;
+                //retVal.Height = Height;
+                //retVal.Width = Width;
+
+                await Store.Save(Name, retVal);
+            }
+            else
+            {
+                throw new Exception(String.Format("Screen {0} does not exist in configuration", Name));
+            }
+
+
+        }
+
+      
+
+    }
+}
