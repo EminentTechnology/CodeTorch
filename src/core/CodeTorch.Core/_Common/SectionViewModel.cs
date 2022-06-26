67514d90200874c2adb5f398f12a4074be05c68b
Philip Ochu <pochu@eminenttechnology.com>
2022-06-25 23:31:02 -0500

    WIP

--- src/core/CodeTorch.Core/_Common/SectionViewModel.cs
+++ src/core/CodeTorch.Core/_Common/SectionViewModel.cs
@@ -15,4 +15,18 @@ namespace CodeTorch.Core
         public IPageModel PageModel { get; private set; }
         public Section Section { get; private set; }
     }
+
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IPageModel pageModel, Section section, Widget widget)
+        {
+            PageModel = pageModel;
+            Section = section;
+            Widget = widget;
+        }
+
+        public IPageModel PageModel { get; private set; }
+        public Section Section { get; private set; }
+        public Widget Widget { get; private set; }
+    }
 }
